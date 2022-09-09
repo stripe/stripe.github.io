9252780b359603a40821d281e09f2f7e60eb50f4
Charlie Gerard <charliegerard@stripe.com>
2022-09-09 12:45:58 -0700

    implement feedback

--- _next/static/tcXCD8R6d79Mf6oJiUiXv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9ac0ee6990901047.css","static/chunks/pages/index-519be54e0ab8b9a2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
